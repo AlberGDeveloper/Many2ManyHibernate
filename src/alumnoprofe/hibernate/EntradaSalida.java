f735236a9ccdd2033761b3e6ae8a26c98cbb2b11
Albersss <alber.garcia.lopez106@gmail.com>
2021-03-15 15:39:43 +0100

    Terminamos E/S

--- src/alumnoprofe/hibernate/EntradaSalida.java
+++ src/alumnoprofe/hibernate/EntradaSalida.java
@@ -4,14 +4,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EntradaSalida {
-	
+
 	public static final int INSERTAR_PROFESOR = 1;
 	public static final int INSERTAR_ALUMNO = 2;
 	public static final int ASOCIAR_PROFESOR_ALUMNO = 3;
 	public static final int LISTAR_PROFESORES = 4;
 	public static final int BUSCAR_PROFESOR = 5;
 	public static final int SALIR = 6;
-
 
 	public static int mostrarMenu() {
 		System.out.println(INSERTAR_PROFESOR + " Insertar profesor");
@@ -20,11 +19,10 @@ public class EntradaSalida {
 		System.out.println(LISTAR_PROFESORES + " Listar profesores");
 		System.out.println(BUSCAR_PROFESOR + " Buscar profesor por nombre");
 		System.out.println(SALIR + " Salir");
-		Scanner sc=new Scanner(System.in);
-		int opcion=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int opcion = sc.nextInt();
 		return opcion;
 	}
-
 
 	public static Profesor pedirProfesor() {
 		Profesor profe = new Profesor();
@@ -35,7 +33,6 @@ public class EntradaSalida {
 		return profe;
 	}
 
-
 	public static Alumno pedirAlumno() {
 		Alumno gafas = new Alumno();
 		Scanner sc = new Scanner(System.in);
@@ -45,41 +42,45 @@ public class EntradaSalida {
 		return gafas;
 	}
 
-
 	public static String mostrarProfesores(List<Profesor> profesores) {
-		for(Profesor p: profesores)
-		{
-			System.out.println(p.getId()+"-"+p.getNombre());
+		for (Profesor p : profesores) {
+			System.out.println(p.getId() + "-" + p.getNombre());
 		}
 		System.out.println("Escriba el nombre del profesor");
-		Scanner sc=new Scanner(System.in);
-		String nombreprofe=sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		String nombreprofe = sc.nextLine();
 		return nombreprofe;
 
 	}
 
 //Método susceptible a eliminar. No pide mostrar ALUMNOS
 	public static String mostrarAlumnos(List<Alumno> listaalumnos) {
-		for(Alumno a: listaalumnos)
-		{
-			System.out.println(a.getId()+"-"+a.getNombre());
+		for (Alumno a : listaalumnos) {
+			System.out.println(a.getId() + "-" + a.getNombre());
 		}
 		System.out.println("Escriba el nombre del alumno");
-		Scanner sc=new Scanner(System.in);
-		String nombrealumno=sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		String nombrealumno = sc.nextLine();
 		return nombrealumno;
 	}
 
-
 	public static String pedirNombreBuscado() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("Introduzca el nombre del profesor a buscar");
+		Scanner sc = new Scanner(System.in);
+		String nombre = sc.nextLine();
+		return nombre;
 	}
 
-
 	public static void mostrarProfeNombre(Profesor profe_buscado) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("NOMBRE: " + profe_buscado.getNombre());
+		System.out.println("ID: " + profe_buscado.getId());
+		List<Alumno> lista_alumnos = profe_buscado.getListaalumnos();
+		for (Alumno a : lista_alumnos) {
+			System.out.println("ID: " + a.getId());
+			System.out.println("NOMBRE ALUMNO: " + a.getNombre());
+
+		}
+
 	}
 
 }
